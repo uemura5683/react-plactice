6ef46fbc04ab643f0fcf678aadc74e2202fb5e39
uemuragame5683 <nobuyoshi5683@gmail.com>
2022-04-06 00:12:06 +0900

    チャットコンテンツ追加!

---
+++ pages/chat.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect, useMemo, useRef } from 'react';
+import Head from 'next/head'
+import db from "../utils/firebase";
+import styles from '../styles/css/Chat.module.css';
+
+const Chat: React.FC = () => {
+  return (
+    <div className={styles.c_contact_confirm}>
+    </div>
+  );
+};
+export default Chat;
